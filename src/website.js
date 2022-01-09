27106167589cdd59b5043f9cc5670395f1f28d08
bmccabe96 <brianmccabe96@gmail.com>
2022-01-09 14:44:14 -0500

    added function to underline the selected tab to website.js

--- src/website.js
+++ src/website.js
@@ -4,8 +4,6 @@ import loadMenu from "./menu";
 import loadContact from "./contact";
 
 
-
-
 function removeContentNode() {
     const content = document.querySelector("#content");
     content.removeChild(content.childNodes[2]); // content section always 3rd element
@@ -14,20 +12,35 @@ function removeContentNode() {
 function loadWebsite(input) {
     if (input === "initial") {
         loadNav();
+        tabSelected("home");
         loadHome();
     }
     else if (input === "home") {
         removeContentNode();
+        tabSelected("home");
         loadHome();
     } else if (input === "menu") {
         removeContentNode();
+        tabSelected("menu");
         loadMenu();
     } else {
         removeContentNode();
+        tabSelected("contact");
         loadContact();
     }
 
-    
+}
+
+function tabSelected(tab) {
+    const tabs = document.querySelector(".tabs").childNodes;
+    for (let i=0; i<tabs.length; i++) {
+        if (tabs[i].classList[0] === tab) {
+            tabs[i].classList.add("selected");
+        }
+        else {
+            tabs[i].classList.remove("selected");
+        }
+    }
 }
 
 export default loadWebsite;
