44eda8590325d4bebec466ce3a3026c969ccd1bd
bmccabe96 <brianmccabe96@gmail.com>
2022-01-09 15:56:01 -0500

    added new font, menu, and contact pages

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,17 @@
+function createParagraph(text) {
+    const p = document.createElement("p");
+    p.textContent = text;
+    return p;
+}
+
 function loadContact() {
     const contact = document.createElement("div");
     contact.className = "content-contact";
-    contact.textContent = "CONTACT TBD";
+
+    contact.appendChild(createParagraph("(123) 456 - 7890"));
+    contact.appendChild(createParagraph("eve@evesleaves.com"));
+    contact.appendChild(createParagraph("123 MacDougal Street"));
+
 
     const content = document.querySelector('#content');
     content.appendChild(contact);
--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,81 @@
+function createParagraph(text) {
+    const p = document.createElement("p");
+    p.textContent = text;
+    return p;
+}
+
 function loadMenu() {
     const menu = document.createElement("div");
     menu.className = "content-menu";
-    menu.textContent = "MENU TBD";
+
+    const looseLeaf = document.createElement("div");
+    looseLeaf.className = "loose-leaf";
+    const looseLeafCat = document.createElement("div");
+    looseLeafCat.className = "category";
+    looseLeafCat.textContent = "Loose Leaf Tea";
+    looseLeaf.appendChild(looseLeafCat);
+    looseLeaf.appendChild(createParagraph("Irish breakfast"));
+    looseLeaf.appendChild(createParagraph("Lady gray"));
+    looseLeaf.appendChild(createParagraph("Brussels breakfast"));
+    looseLeaf.appendChild(createParagraph("Vanilla almond"));
+    looseLeaf.appendChild(createParagraph("Earl gray"));
+    looseLeaf.appendChild(createParagraph("Chamomile mint"));
+    looseLeaf.appendChild(createParagraph("Lemongrass"));
+
+    const sigDrinks = document.createElement("div");
+    sigDrinks.className = "sig-drinks";
+    const sigDrinksCat = document.createElement("div");
+    sigDrinksCat.className = "category";
+    sigDrinksCat.textContent = "Signature Drinks";
+    sigDrinks.appendChild(sigDrinksCat);
+    sigDrinks.appendChild(createParagraph("Dirty chai"));
+    sigDrinks.appendChild(createParagraph("Lavender tea latte"));
+    sigDrinks.appendChild(createParagraph("Rose tea latte"));
+    sigDrinks.appendChild(createParagraph("Cinnamon oatmilk tea latte"));
+    sigDrinks.appendChild(createParagraph("Marzipan tea latte"));
+    sigDrinks.appendChild(createParagraph("Raspberry ribbon tea latte"));
+
+    const nonTea = document.createElement("div");
+    nonTea.className = "non-tea";
+    const nonTeaCat = document.createElement("div");
+    nonTeaCat.className = "category";
+    nonTeaCat.textContent = "Non Tea";
+    nonTea.appendChild(nonTeaCat);
+    nonTea.appendChild(createParagraph("Hot chocolate"));
+    nonTea.appendChild(createParagraph("Vanilla steamer"));
+    nonTea.appendChild(createParagraph("Chestnut steamer"));
+
+    const pastries = document.createElement("div");
+    pastries.className = "pastries";
+    const pastriesCat = document.createElement("div");
+    pastriesCat.className = "category";
+    pastriesCat.textContent = "Pastries";
+    pastries.appendChild(pastriesCat);
+    pastries.appendChild(createParagraph("Almond croissant"));
+    pastries.appendChild(createParagraph("Pain au chocolat"));
+    pastries.appendChild(createParagraph("Princess cake"));
+    pastries.appendChild(createParagraph("Sacher torte"));
+    pastries.appendChild(createParagraph("Rainbow cake"));
+    pastries.appendChild(createParagraph("Jammy biscuits"));
+    pastries.appendChild(createParagraph("Raspberry millefeuille"));
+    pastries.appendChild(createParagraph("Five perfect macarons"));
+
+    const breads = document.createElement("div");
+    breads.className = "breads";
+    const breadsCat = document.createElement("div");
+    breadsCat.className = "category";
+    breadsCat.textContent = "Breads";
+    breads.appendChild(breadsCat);
+    breads.appendChild(createParagraph("Rye bagel"));
+    breads.appendChild(createParagraph("Bialy"));
+    breads.appendChild(createParagraph("Ciabatta breaksticks"));
+    breads.appendChild(createParagraph("Garlic bread"));
+
+    menu.appendChild(looseLeaf);
+    menu.appendChild(sigDrinks);
+    menu.appendChild(nonTea);
+    menu.appendChild(pastries);
+    menu.appendChild(breads);
 
     const content = document.querySelector('#content');
     content.appendChild(menu);
